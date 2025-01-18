859aeb3dfd90be3c76ed25c9398450fe6e7153f7
diemyolo <quydiem2015@gmail.com>
2025-01-18 19:19:50 +0700

    initial create

---
+++ movewise-api/src/main/java/com/movewise/movewise_api/Utilities/DemoUtilities.java
@@ -0,0 +1,5 @@
+package com.movewise.movewise_api.Utilities;
+
+public class DemoUtilities {
+
+}
---
+++ movewise-api/src/test/java/com/movewise/movewise_api/MovewiseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movewise.movewise_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovewiseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
