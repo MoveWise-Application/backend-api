7f6cdcf32a7466015b47f8713eb89fd9e47725de
DaoHuongThao <daothao148@gmail.com>
2025-01-20 22:27:08 +0700

    config db connection

--- movewise-api/src/main/java/com/movewise/movewise_api/Model/AssignmentGroup.java
+++ movewise-api/src/main/java/com/movewise/movewise_api/Model/AssignmentGroup.java
@@ -1,5 +1,25 @@
 package com.movewise.movewise_api.Model;
 
-public class AssignmentGroup {
+import com.movewise.movewise_api.Model.Enumberable.GroupStatus;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Table(name = "assignment_group")
+public class AssignmentGroup extends BaseEntity {
+    @Enumerated(EnumType.ORDINAL)
+    private GroupStatus groupStatus;
 }
